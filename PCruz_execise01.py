9535419d99add58623815129564bee84026792be
pinkerxy <31789031+pinkerxy@users.noreply.github.com>
2017-09-18 23:09:39 +0800

    Exercise 01

---
+++ PCruz_execise01.py
@@ -0,0 +1,9 @@
+#Challenge
+bill = input('How much is your total bill?')
+payment = input('How much is your payment?')
+bill= float(bill)
+payment= float(payment)
+change= payment - bill
+
+#print('Hi!  Your change is' + change)
+print ('Your change is ' , change)
